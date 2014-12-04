6fddbfe6fd9fc1c7dc59decfebf8ec9034ba3ac2
Ariel <ariel.s.moraes@gmail.com>
2014-12-04 21:25:26 -0200

    Respawn da bola de fogo

---
+++ Assets/scripts/Fireball.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireball : MonoBehaviour {
+
+	private float posicaoY = 0f;
+	private Transform player;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject,2f);
+		rigidbody2D.AddForce (transform.up * 400f);
+		posicaoY = transform.position.y;
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (posicaoY > transform.position.y) { //Descendo
+			transform.eulerAngles = new Vector2(180, 0);
+		}
+		posicaoY = transform.position.y;
+	}
+
+	void OnCollisionEnter2D (Collision2D colisor) {
+		if (colisor.gameObject.tag == "Player") {
+			player.position = new Vector3(0, 0, 0);
+		}
+	}
+}
