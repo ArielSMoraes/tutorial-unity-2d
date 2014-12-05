e996f5dbb79dc919b2260828a8034a2492155f0b
Ariel <ariel.s.moraes@gmail.com>
2014-12-04 23:49:13 -0200

    Prefab de Plataformas

---
+++ Assets/scripts/Fall.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fall : MonoBehaviour {
+
+	public float duracaoCair;
+	private float tempo;
+	private bool pisou = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pisou) {
+			tempo+= Time.deltaTime;
+			
+			if (tempo >= duracaoCair) {
+				gameObject.AddComponent("Rigidbody2D");
+				Destroy (gameObject,2f);
+				tempo = 0f;
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D colisor) {
+		if (colisor.gameObject.name == "Player") {
+			pisou = true;
+		}
+	}
+
+}
--- Assets/scripts/Lava.cs
+++ Assets/scripts/Lava.cs
@@ -3,11 +3,8 @@ using System.Collections;
 
 public class Lava : MonoBehaviour {
 
-	private Transform player;
-
 	// Use this for initialization
 	void Start () {
-		player = GameObject.FindGameObjectWithTag ("Player").transform;
 	}
 	
 	// Update is called once per frame
@@ -17,7 +14,7 @@ public class Lava : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D colisor) {
 		if (colisor.gameObject.tag == "Player") {
-			player.position = new Vector3(0, 0, 0);
+			Application.LoadLevel(Application.loadedLevel);
 		}
 	}
 }
